a91e0bed8bc09437d9e10befe895c4c5bc21196a
Martijn <Martijn@10.92.33.120>
2013-04-16 11:17:55 +0200

    exc

---
+++ ogpTaak/src/asteroids/InvalidPositionException.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package asteroids;
+
+/**
+ * A class of exceptions signaling invalid positions.
+ * 
+ * @version	1.0
+ * @author 	Martijn Boussé, Wout Vekemans
+ * 
+ *
+ */
+@SuppressWarnings("serial")
+public class InvalidPositionException extends RuntimeException {
+	/**
+	 * Initialize this new invalid position exception.
+	 * 
+	 * @effect	...
+	 * 			| super()
+	 */
+	public InvalidPositionException() {
+		super();
+	}
+}

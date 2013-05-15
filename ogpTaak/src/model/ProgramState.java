83d11b25ac00150732b6052c7ae762478c03cc2a
Martijn <Martijn@10.92.33.12>
2013-05-15 14:25:22 +0200

    program state

---
+++ ogpTaak/src/model/ProgramState.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import be.kuleuven.cs.som.annotate.Basic;
+import be.kuleuven.cs.som.annotate.Immutable;
+
+import types.Type;
+
+import gameObjects.Ship;
+
+public class ProgramState {
+	
+	public ProgramState(Ship self, Map<String,Type> globals) {
+		this.self = self;
+		this.globals = globals;
+	}
+	
+	@Basic @Immutable
+	public Ship getSelf() {
+		return this.self;
+	}
+	
+	private final Ship self;
+	
+	@Basic
+	public Map<String,Type> getGlobals() {
+		return new HashMap<String,Type>(globals);
+	}
+	
+	private final Map<String,Type> globals;
+	
+	
+	
+
+}
